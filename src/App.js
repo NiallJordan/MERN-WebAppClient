e6c6da11672acb3ed8b491ccea65451b079e61b3
Niall Jordan <niall.jordan2012@gmail.com>
2019-12-06 19:00:00 +0000

    Static football club app completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/header/";
+import ClubList from "./components/clubComponents/clubList";
+import FilterControls from "./components/filterControls/";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const sample = {
+      name: 'Liverpool',
+      picture: {logo:'./logo.png'},
+      league: 'Premier League',
+      placeInLeague: 1
+    }
+
+    const clubs = [sample, sample, sample, sample, sample];
+
+    return (
+      <div className="jumbotron">
+        <Header noClubs={10} />
+        <FilterControls />
+        <ClubList clubs={clubs} />
+      </div>
+    );
+  }
 }
 
 export default App;
