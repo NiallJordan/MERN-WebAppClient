c83c3a09f4b78080b41b9e54687d80da433d16e2
Niall Jordan <niall.jordan2012@gmail.com>
2019-12-07 13:35:42 +0000

    Routing setup completed

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,8 +1,11 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope, faPhone, faMedal } from "@fortawesome/free-solid-svg-icons";
-
-library.add(
+import {
   faEnvelope,
   faPhone,
+  faHome,
+  faBirthdayCake,
+  faArrowCircleLeft,
   faMedal
-  );
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faEnvelope, faPhone, faHome, faBirthdayCake, faMedal,faArrowCircleLeft);
--- stories/index.js
+++ stories/index.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { MemoryRouter, Route } from "react-router";
 
 import Header from '../src/components/header';
 import Filter from '../src/components/filterControls';
 import Club from '../src/components/clubComponents/club';
 import ClubList from '../src/components/clubComponents/clubList';
 
-import { action } from '@storybook/addon-actions';
 
 const sample = {
   name: 'Liverpool',
   picture: {logo:'./logo.png'},
   league: 'Premier League',
   placeInLeague: 1,
-  phone: '0151 906 1892'
+  phone: '0151 906 1892',
+  location:{
+    city:"Liverpool",
+    country:"England"
+  },
+  stadium:{
+    name:"Anfield",
+    capacity:54074
+  },
+  numberOfPlayers:37,
+  yearEstablished: 1892,
+  "login":{
+    "uuid":"9813dc96-18f5-11ea-b8f0-2e728ce88125",
+    "username":"liverpool-admin",
+    "password":"sixTimes",
+    "salt":"iLikeCake",
+    "md5": "78671b38791a07d80315a2865c83ec3d",
+    "sha1": "	b74353cd5155fad7cf5cb86913bc46964a7b1324",
+    "sha256":"713bc71624de6b21c41d6806ea8addadf9d8ceb79e17b5d4bedb754a2c5a0140"
+
+  },
+  "dob":{
+    "date": "1969-05-04",
+    "age":"50"
+  },
+  "registered":{
+    "date":"2017-12-7",
+    "age":2
+  }
 }
+
 //Header Story
 storiesOf("Football Club App/Header", module).add("default", () => (
   <Header noClubs={10} />
@@ -25,15 +55,12 @@ storiesOf("Football Club App/Filter Controls", module).add("default", () => (
   <Filter onUserInput={action('Search criteria changes')} />
 ));
 
-storiesOf("Football Club App/Club", module).add("default", () => (
-  <Club club={sample}/>
-));
+storiesOf("Football Club App/Club", module).addDecorator(story => ( <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>))
+.add("default", () => ( <Club club={sample} deleteHandler={action('Delete confirmed')}/>));
 
-storiesOf("Football Club App/Club List", module).add("default", () => {
+storiesOf("Football Club App/Club List", module).addDecorator(story => ( <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>))
+.add("default", () => {
   const samples = [sample, sample, sample, sample, sample]
   return <ClubList clubs={samples}/>
 });
 
-storiesOf("Football Club App/Club", module).add("default", () => (
-  <Club club={sample} deleteHandler={action('Delete confirmed') }/>
-));
