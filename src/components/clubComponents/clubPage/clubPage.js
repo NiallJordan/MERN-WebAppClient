92b83efab262e4ef515faa4f95216935df621b0b
Niall Jordan <niall.jordan2012@gmail.com>
2019-12-07 17:32:34 +0000

    Added nested route for more info

--- src/components/clubComponents/clubPage/clubPage.js
+++ src/components/clubComponents/clubPage/clubPage.js
@@ -1,8 +1,8 @@
 import React, {Fragment} from "react";
-import { withRouter } from "react-router-dom";
 import ClubPublic from "../clubPublic/clubPublic";
 import api from "../../../dataStore/stubAPI";
 import ClubMoreInfo from "../clubMoreInfo/index";
+import { withRouter, Route, Link } from "react-router-dom";
 
 const ClubPage = props => {
   const { id } = props.match.params;
@@ -12,7 +12,9 @@ const ClubPage = props => {
        {club ? (
          <Fragment>
            <ClubPublic club= {club}/>
-           <ClubMoreInfo club={club}/>
+           {!props.history.location.pathname.endsWith("/moreInfo") && (<Link class="btn btn-primary active" to={`/clubs/${id}/moreInfo`}>See More Info</Link>
+           )}
+           <Route path={ `/clubs/:id/moreInfo`} render={(props) => <ClubMoreInfo {...props} club={club}/>}/>
         </Fragment>
        ):(
          <p>Waiting for club details</p>
